08f9dcca54ff3d50b90a72eca78554a301247694
Yawen Zhao <77918358+Yawen-Zhao-820@users.noreply.github.com>
2021-04-12 18:05:16 +1000

    Upload 0412
    Upload original files

---
+++ js/script.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+	 
+	/* slick img slides */
+
+	var slick = {
+		dots: true,
+		arrows:true,
+        infinite: true,
+        pauseOnHover: true,
+        pauseOnFocus: false,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+		speed: 800 
+	}
+
+	$(".img-slides").slick(slick);
+	
+    $(".img-slides-02").slick(slick);
+
+	$(".img-slides-03").slick(slick);
+
+	$(".img-slides-04").slick(slick);
+
+	$(".img-slides-05").slick(slick);
+
+	$(".img-slides-06").slick(slick);
+
+	$(".img-slides-07").slick(slick);
+
+	$(".img-slides-08").slick(slick);
+
+	$(".img-slides-09").slick(slick);
+
+	$(".img-slides-10").slick(slick);
+
+	$(".img-slides-11").slick(slick);
+
+});
+
+
